b7aeb0f4d0f0bf422df2cb871cd7d86a543408e2
Nikhil Kadyan <nitronkd@gmail.com>
2018-04-06 17:34:22 +0530

    Adding About section and Some Major Overhaul

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  @Input()  themeData: any;
   constructor() { }
 
   ngOnInit() {
